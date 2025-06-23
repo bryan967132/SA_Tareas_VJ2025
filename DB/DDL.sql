ba5eb050a95cd4593c15458e1c9ae6c73a5719db
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2025-06-22 22:24:47 -0600

    [UPDATE] Se agregó modelo de base de datos y documentación

---
+++ DB/DDL.sql
@@ -0,0 +1,54 @@
+DROP DATABASE IF EXISTS sa_t3;
+CREATE DATABASE IF NOT EXISTS sa_t3;
+
+CREATE TABLE IF NOT EXISTS sa_t3.configuration_items (
+    id INT PRIMARY KEY NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    tipo VARCHAR(25) NOT NULL CHECK (tipo IN ('servidor', 'base de datos', 'aplicacion', 'otros')),
+    descripcion VARCHAR(200),
+    numero_serie VARCHAR(15),
+    version VARCHAR(15),
+    fecha_adquisicion DATE,
+    estado_actual VARCHAR(15) NOT NULL CHECK (estado_actual IN ('activo', 'inactivo', 'mantenimiento', 'retirado')),
+    ubicacion_fisica VARCHAR(100),
+    propietario_responsable VARCHAR(100),
+    niveles_seguridad VARCHAR(15),
+    cumplimiento VARCHAR(15),
+    estado_configuracion VARCHAR(15),
+    numero_licencia VARCHAR(15),
+    fecha_vencimiento DATE,
+    ambiente VARCHAR(15) NOT NULL CHECK (ambiente IN ('DEV', 'QA', 'PROD')),
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS sa_t3.cambios_ci (
+    id INT PRIMARY KEY NOT NULL,
+    fecha DATE NOT NULL,
+    descripcion VARCHAR(100),
+    ci_id INT NOT NULL,
+    FOREIGN KEY (ci_id) REFERENCES configuration_items(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS sa_t3.documentacion_ci (
+    id INT PRIMARY KEY NOT NULL,
+    url_documentacion VARCHAR(200) NOT NULL,
+    ci_id INT NOT NULL,
+    FOREIGN KEY (ci_id) REFERENCES configuration_items(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS sa_t3.incidentes_problemas_ci (
+    id INT PRIMARY KEY NOT NULL,
+    enlace VARCHAR(255) NOT NULL,
+    ci_id INT NOT NULL,
+    FOREIGN KEY (ci_id) REFERENCES configuration_items(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS sa_t3.relaciones_ci (
+    id INT PRIMARY KEY NOT NULL,
+    tipo_relacion VARCHAR(15) NOT NULL,
+    ci_padre_id INT NOT NULL,
+    ci_hijo_id INT NOT NULL,
+    FOREIGN KEY (ci_padre_id) REFERENCES configuration_items(id) ON DELETE CASCADE,
+    FOREIGN KEY (ci_hijo_id) REFERENCES configuration_items(id) ON DELETE CASCADE
+);
